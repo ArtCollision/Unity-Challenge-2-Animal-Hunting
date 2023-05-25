aa4a01a9cbc9a07ca4d2f94790fc5a68783c9e7b
ArtCollision <candice@artcollision.ca>
2023-05-25 16:21:53 -0400

    Destroy animals offscreen
    In the Destroyoutofbound script, I added a new variable and wrote and else if statement, so the animals will disappear then they will go lower screen

--- DestroyOutOfBounds.cs
+++ DestroyOutOfBounds.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,10 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > 30)
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
